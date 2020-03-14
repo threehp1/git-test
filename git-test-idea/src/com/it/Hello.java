e13704987c5fc99593a4a518d8230dd465c5cacd
hp <1142925618@qq.com>
2020-03-14 16:56:57 +0800

    第二次提交的更改项目

--- git-test-idea/src/com/it/Hello.java
+++ git-test-idea/src/com/it/Hello.java
@@ -4,4 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+    public void test(){
+        System.out.println("hhhh");
+    }
 }
